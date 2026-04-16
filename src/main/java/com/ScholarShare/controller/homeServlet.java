3a9cd8e79e2ddf63c66393d7e098068afb5f2ad1
Hemanta Rana <magarhemanta145@gmail.com>
2026-04-16 11:39:49 +0545

    fix the default home page

--- src/main/java/com/ScholarShare/controller/homeServlet.java
+++ src/main/java/com/ScholarShare/controller/homeServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet({"", "/home"})
+@WebServlet("/home")
 public class homeServlet extends HttpServlet {
 
     @Override
