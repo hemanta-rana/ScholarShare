4292de6e7af8990944b7dadedac27fe0c9f612f8
RTP987654 <rupantilijapun@gmail.com>
2026-05-16 15:02:27 +0545

    override getAllApproved done.

--- src/main/java/com/ScholarShare/dao/daoImpl/ResourceDaoImpl.java
+++ src/main/java/com/ScholarShare/dao/daoImpl/ResourceDaoImpl.java
@@ -3,7 +3,12 @@ package com.ScholarShare.dao.daoImpl;
 import com.ScholarShare.dao.ResourceDao;
 import com.ScholarShare.entity.Resource;
 import com.ScholarShare.entity.Topic;
+import com.ScholarShare.util.DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,5 +28,55 @@ public class ResourceDaoImpl implements ResourceDao {
         return false;
     }
 
+    @Override
+    public List<Resource> getAllApproved() {
+        List<Resource> resources = new ArrayList<>();
+        Connection connection = null;
+        try {
+            connection = DatabaseConnection.getConnection();
+            String sql ="SELECT * FROM resources WHERE status = 'approved' ORDER BY upload_date DESC";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                Resource resource = new Resource();
+                resource.setResourceId(rs.getInt("resource_id"));
+                resource.setUserId(rs.getInt("user_id"));
+                resource.setTopicId(rs.getInt("topic_id"));
+                resource.setTitle(rs.getString("title"));
+                resource.setDescription(rs.getString("description"));
+                resource.setFilePath(rs.getString("file_path"));
+                resource.setResourceType(rs.getString("resource_type"));
+                resource.setStatus(rs.getString("status"));
+                resource.setSelfDeclaration(rs.getBoolean("self_declaration"));
+                resource.setUploadDate(rs.getTimestamp("upload_date"));
+                resources.add(resource);
+            }
+        } catch (SQLException e){
+            System.out.println("Cannot get all approved resources "+e.getMessage());
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+        return resources;
+    }
 
+    @Override
+    public List<Resource> getByFaculty(int facultyId){};
+
+    @Override
+    public List<Resource> getBySubject(int subjectId){};
+
+    @Override
+    public List<Resource> getByTopic(int topicId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Resource> search(String keyword) {
+        return List.of();
+    }
+
+    @Override
+    public Resource getById(int resourceId) {
+        return null;
+    }
 }
